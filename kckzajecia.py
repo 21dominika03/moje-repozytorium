5bfcc08bada425d392e923531d95c6642d152122
Dominika <dominisiamika@gmail.com>
2018-11-19 10:43:34 +0100

    hejhej

---
+++ kckzajecia.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# ~/python/01_hello.py
+
+# deklarujemy i inicjalizujemy zmienne
+aktRok = 2014
+pythonRok = 1989
+# obliczamy wiek Pythona
+wiekPythona = aktRok - pythonRok
+
+# pobieramy dane
+imie = raw_input('Jak się nazywasz? ')
+wiek = int(raw_input('Ile masz lat? '))
+
+print "Witaj w moim świecie",imie
+print "Mów mi Python, mam",wiekPythona,"lat."
+
+# instrukcja warunkowa
+if wiek > wiekPythona:
+	print 'Jesteś starszy ode mnie.'
+else:
+	print 'Jesteś młodszy ode mnie.'
